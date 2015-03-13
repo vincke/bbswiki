2a190cdfb730669c229265ca1ef6bae9a74b847b
jusescn <jusescn@163.com>
2015-03-13 16:32:30 +0800

    1

---
+++ 黑洞薇薇/javascriptV.lookByAuthor('美色当前')--__ty_vip_fn_look_by_author.js
@@ -0,0 +1,156 @@
+tail -f /opt/jakarta-tomcat-5.0.30/logs/catalina.out
+2012-9-28
+
+122.228.243.22 dynamic.12306.cn
+#61.183.42.94   dynamic.12306.cn
+5点多开始放票.txt
+
+2012-7-26
+KW7142情趣内衣
+voggly@sis
+http://www.ctdisk.com/file/8500762
+http://www.ctdisk.com/file/8491475
+http://www.ctdisk.com/file/8500965
+http://www.ctdisk.com/file/8501025
+http://174.127.195.163/bbs/thread-4485420-1-2.html
+http://174.127.195.163/bbs/thread-4492179-1-2.html
+
+2012-4-12
+http://se0322.ctdisk.com/u/275887/817174/1
+
+2012-3-31
+http://www.ctdisk.com/u/759932/991867/2
+http://www.ctdisk.com/u/743867/
+http://www.ctdisk.com/u/621606/841345
+http://www.ctdisk.com/u/449359/
+http://www.ctdisk.com/u/153673/179321
+http://www.ctdisk.com/u/743867/1017573/2
+http://www.ctdisk.com/u/544701/
+
+Top_Girl_第一季 http://u.115.com/file/a9jek01p
+Top_Girl_第二季 http://u.115.com/file/a9j09u4h
+Top_Girl_第三季 http://u.115.com/file/blwcfd28
+
+
+2012-2-24
+http://115.com/file/aq1hdp1l#
+http://115.com/file/aq7a738f#
+http://u.115.com/file/bh5g0ni6#
+http://u.115.com/file/t014f62386
+http://u.115.com/file/t0235af193#
+http://dl.dbank.com/c0kybvhf9i
+http://115.com/file/clj18p4k#
+http://115.com/file/dn10witw#
+http://dl.dbank.com/c0yu8wxl9j
+http://115.com/file/bhf8cc96#
+http://115.com/file/bewtdxxg
+http://dl.dbank.com/c0zszyqkcq
+http://dl.dbank.com/c0fhx0uonk
+http://dl.dbank.com/c0tv47tjgq
+
+2011-9-26
+http://www.tudou.com/programs/view/DtrrkSKEacc/
+2011-9-8
+http://player.youku.com/player.php/sid/XMjI4OTk5NDY0/v.swf
+http://player.youku.com/player.php/sid/XMjI5NDUwNDA0/v.swf
+http://player.youku.com/player.php/sid/XMjI5OTUxMjE2/v.swf
+http://v.youku.com/v_show/id_XMjI5MDc1NjA0.html
+http://v.youku.com/v_show/id_XMjI5Mjg1OTM2.html
+http://player.youku.com/player.php/sid/XMjI5MzMyMzA4/v.swf
+
+ed2k://|file|%E6%9D%A8%E6%A3%8B%E6%B6%B5.avi|49500988|CF0077329BF16D36CFE60C03B776E71C|
+
+2011-6-28
+Making an all javascript blog with #CouchDB and #Node.js
+http://blog.nosqlfan.com/html/2222.html
+http://www.screenr.com/418s
+http://www.screenr.com/nY7s
+2011-01-24
+http://itunes.apple.com/app/chop-chop-soccer/id382675103?mt=8
+http://itunes.apple.com/cn/app/qqlive-hd/id407925512?mt=8
+http://itunes.apple.com/cn/app/id403927669?mt=8=8
+http://itunes.apple.com/cn/app/id412378447?mt=8 友录通讯录开发商：Youlu Co., Ltd.
+http://itunes.apple.com/app/101-in-1-gameutilities-10/id340181305?mt=8 101 in 1 GameUtilities - 10 Games & 91 Utilities
+
+PSP
+2011-01-21
+http://u.115.com/file/f08ed76199 愤怒的小鸟 解压密码：psp.duowan.com
+http://u.115.com/file/f3edf2027  《皇牌空战X2》简体中文版下载（BT+115）
+http://u.115.com/file/f3c3305efd  《皇牌空战X2》简体中文版下载（BT+115）
+
+怪物猎人P3 简体中文汉化版 解压密码 psp.duowan.com
+http://u.115.com/file/f14f410b4f
+http://u.115.com/file/f167ebb0be
+
+2011-01-19
+http://u.115.com/file/f12b371fa5  解压密码』p t b u s.com （把空去掉！）
+http://itunes.apple.com/cn/app/id414874814?mt=8
+
+IPONE
+http://u.115.com/file/f2f35eca2c  AngryBirds PC版
+http://bbs.weiphone.com/read-htm-tid-1417580.html 精品摄影软件合集67MB打包下载
+http://bbs.weiphone.com/read-htm-tid-1424591.html 丧尸围城-DEAD RISING MOBILE
+http://bbs.weiphone.com/read-htm-tid-1449308.html 劳拉与光明守护者Lara Croft and the Guardian of Light
+http://bbs.weiphone.com/read-htm-tid-1121060.html Gangstar: Miami Vindication孤胆车神：雄霸迈阿密
+http://bbs.weiphone.com/read-htm-tid-1449189.html Real Racing 2
+http://bbs.weiphone.com/read-htm-tid-1469355.html 极品飞车:热血追踪
+
+http://www.51ipa.com/games/relax/Dark-Nebula-Episode-Two-iPhone.html ark Nebula - Episode Two v1.1 - 黑暗星云第二章 
+http://www.51ipa.com/games/relax/Amateur-Surgeon-iPad-Edition.html Amateur Surgeon iPad Edition v1.0.7 - 疯狂外科医生 
+http://www.51ipa.com/games/action/Guerrilla-Bob-HD-iPad.html Guerrilla Bob HD v1.0 - 游击队员鲍比 
+http://www.51ipa.com/games/chess/Carcassonne-iPad-iPhone.html Carcassonne v2.1 - 卡卡颂
+
+http://www.51ipa.com/games/chess/Mahjong-World-iPhone.html Mahjong World v1.1.3 - 麻将天下 中文版
+http://www.51ipa.com/games/strategy/Gas-Tycoon-2-HD-iPad.html Gas Tycoon 2 HD v1.5.3 - 天然气大亨2 
+http://www.51ipa.com/games/action/CF-Defense-HD-iPad.html CF Defense HD v1.4 - 穿越火线的防御
+http://www.51ipa.com/games/strategy/ROMANCE-OF-THE-THREE-KINGDOMS-2-iPhone.html  ROMANCE OF THE THREE KINGDOMS 2 v1.0 - 三国志2 
+http://www.51ipa.com/games/sports/PES-2011-Pro-Evolution-Soccer-iPad-iPhone.html PES 2011 - Pro Evolution Soccer v1.2.0 - 实况足球2011 
+http://www.51ipa.com/games/action/Mirrors-Edge-for-iPad.html Mirror's Edge? for iPad v1.1.38 - 镜之边缘 
+http://www.51ipa.com/games/sports/FIFA-11-by-EA-SPORTS-for-iPad.html FIFA 11 by EA SPORTS? for iPad v1.0.0 - FIFA 11 
+http://www.51ipa.com/games/relax/Spirits-iPhone.html Spirits v1.0.1 - 风之精灵
+http://www.51ipa.com/games/strategy/My-Kingdom-for-the-Princess-2-iPad.html My Kingdom for the Princess2 v2010.11.24 - 公主护卫
+http://www.51ipa.com/games/relax/FastBall-2-iPhone.html FastBall 2 v1.3 - 跳跳球2
+http://www.51ipa.com/games/strategy/Supermarket-Mania-HD-iPad.html Supermarket Mania? HD v1.1 - 疯狂超市 
+http://www.51ipa.com/games/strategy/Farm-Frenzy-2-HD-iPad.html Farm Frenzy 2 HD v1.3 - 疯狂的农场
+http://www.51ipa.com/games/strategy/Smurfs-Village-iPad-iPhone.html Smurfs' Village v1.0.1 - 蓝精灵村庄
+http://www.51ipa.com/games/strategy/Supermarket-Management-HD-iPad.html Supermarket Management HD v1.1 - 美女超市经理 
+http://www.51ipa.com/games/strategy/Jean-Boutique-HD-iPad.html Jean's Boutique HD v1.1 - 精品时装店
+http://www.51ipa.com/games/strategy/Hotel-Dash-Deluxe-iPad.html Hotel Dash Deluxe v1.0 - 美女豪华饭店
+http://www.51ipa.com/games/strategy/COMMAND-CONQUER-RED-ALERT-iPhone.html COMMAND & CONQUER? RED ALERT? v1.1.1 - 红色警
+
+DONE
+http://bbs.weiphone.com/read-htm-tid-1464567.html 亲爱的》官方中文版
+http://bbs.weiphone.com/read-htm-tid-1424947.html Dungeon Hunter 2 地下城猎人2
+http://bbs.weiphone.com/read-htm-tid-1149753.html 《Real Soccer 2011》世界足球
+http://bbs.weiphone.com/read-htm-tid-1465985.html ATTLEFIELD: BAD COMPANY? 2 (World) - 战地风云：叛逆连队2
+http://bbs.weiphone.com/read-htm-tid-1424571.html 首款虚幻3引擎ARPG大作.无尽之剑-Infinity Blade
+http://bbs.weiphone.com/read-htm-tid-1464646.html Asphalt 6：Adrenaline，沥青赛车6：火线追击
+http://bbs.weiphone.com/read-htm-tid-1460972.html 27000+天下美食1.2版
+http://bbs.weiphone.com/read-htm-tid-1451896.html N.O.V.A.2 近地联盟先遣队
+http://bbs.weiphone.com/read-htm-tid-1458130.html 高德导航4.3实景下载
+http://bbs.weiphone.com/read-htm-tid-1456629.html 高德4.3.1-----自购自破纯净版
+http://bbs.weiphone.com/read-htm-tid-1153933.html 高德》【4.2.1】下载
+http://itunes.apple.com/cn/app/id411411466?mt=8   三国群殴传
+http://bbs.weiphone.com/read-htm-tid-1465199.html 口袋上帝-Pocket God
+http://u.115.com/file/f680ff181e 《暖暖温泉乡》完全汉化修正版
+http://u.115.com/file/t4b08626fb Health Box v1.1 家庭医药箱  最新版
+http://u.115.com/file/t56f00a8d7 4000精美家常菜 v6.1 最新版 
+http://u.115.com/file/t150f5b53a Chop Chop ninja  v1.5 
+http://u.115.com/file/t5ebd5de48 NBA ELITE 最新版 v1.01 
+http://u.115.com/file/t9805f5ea6 史瑞克 
+http://u.115.com/file/f261034a61 Hit the apple v2.1 
+http://u.115.com/file/t52da211e6 Revolt 1.0
+http://u.115.com/file/t660a1c306 海岸突防 v1.3 最新版 
+http://u.115.com/file/t66c9463a1  卡通战争2：英雄
+http://bbs.weiphone.com/read-htm-tid-1433480.html 卡通战争2 英雄 圣诞版 Cartoon Wars 2 Heroes 
+http://bbs.weiphone.com/read-htm-tid-1449025.html Shadow Guardian-影子守卫
+http://bbs.weiphone.com/read-htm-tid-1449270.html 战地:叛逆联队2-Battlefield: Bad Company 2
+
+http://www.51ipa.com/multimedia/imageditor/Photo_fx_iPhone.html Photo fx v4.0 - 图片滤镜处理
+http://www.51ipa.com/multimedia/imageditor/Photo-fx-Ultra-iPad.html Photo fx Ultra v2.0.1 - 图片滤镜处理
+http://www.51ipa.com/multimedia/avplayer/Xunlei-Kankan-HD-iPad.html 迅雷看看 HD v1.0.1
+http://www.51ipa.com/multimedia/photo/ProCamera-GeoTagging-iPhone.html ProCamera + GeoTagging v3.0 - 专业相机
+http://www.51ipa.com/multimedia/photo/Hipstamatic-iPhone.html Hipstamatic v185 - 创意相机 完整版
+http://www.51ipa.com/multimedia/photo/Camera-Plus-Pro-iPhone.html Camera Plus Pro v2.6 - 全能专业照相机
+http://www.51ipa.com/multimedia/photo/Camera-the-ultimate-photo-app-iPhone.html Camera+ the ultimate photo app v2.0 - 超强相机
+http://www.51ipa.com/games/relax/Fish-Ball-iPad-iPhone.html Fish Ball v1.2 - 鱼蛋大王 圣诞节特别版
